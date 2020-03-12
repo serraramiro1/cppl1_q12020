a84e58e8629b8237f08e3ed9886f759de3214a63
Ramiro Serra <serraramiro@ekumenlabs.com>
2020-03-12 11:09:31 -0300

    Add Matrix3 implementation

--- course/include/isometry.h
+++ course/include/isometry.h
@@ -8,7 +8,6 @@ namespace cppcourse {
 
 class Vector3 {
 public:
-
   Vector3(const std::initializer_list<double> &rhs);
   Vector3(const double &x = 0., const double &y = 0., const double &z = 0.)
       : x_(x), y_(y), z_(z){};
@@ -50,6 +49,72 @@ inline std::ostream &operator<<(std::ostream &ss,
                                 const cppcourse::Vector3 &vec) {
   return ss << "(x: " << vec.x() << ", y: " << vec.y() << ", z: " << vec.z()
             << ")";
+}
+
+class Matrix3 {
+public:
+  Matrix3() {}
+  Matrix3(const std::initializer_list<double> &rhs);
+  Matrix3(const Matrix3 &rhs);
+  Matrix3(const Vector3 &first, const Vector3 &second, const Vector3 &third) {
+    rows_[0] = first;
+    rows_[1] = second;
+    rows_[2] = third;
+  }
+  Matrix3(Matrix3 &&rhs) {
+    rows_[0] = std::move(rhs.rows_[0]);
+    rows_[1] = std::move(rhs.rows_[1]);
+    rows_[2] = std::move(rhs.rows_[2]);
+  }
+  Matrix3 operator-(const Matrix3 &other) const;
+  Matrix3 operator+(const Matrix3 &other) const;
+  Matrix3 operator*(const double &other) const;
+  Matrix3 operator*(const Matrix3 &other) const;
+  Matrix3 operator*(const Vector3 &other) const;
+  Matrix3 operator/(const Matrix3 &other) const;
+  Matrix3 &operator-=(const Matrix3 &rhs);
+  Matrix3 &operator+=(const Matrix3 &rhs);
+  Matrix3 &operator=(const Matrix3 &rhs) {
+    this->rows_[0] = rhs.row(0);
+    this->rows_[1] = rhs.row(1);
+    this->rows_[2] = rhs.row(2);
+    return *this;
+  };
+  Matrix3 &operator=(const Matrix3 &&rhs) {
+    this->rows_[0] = rhs.row(0);
+    this->rows_[1] = rhs.row(1);
+    this->rows_[2] = rhs.row(2);
+    return *this;
+  };
+  Vector3 &operator[](const int row_n) { return row(row_n); };
+  const Vector3 &operator[](const int row_n) const { return row(row_n); };
+
+  const Vector3 &row(int index) const { return rows_[index]; }
+  Vector3 &row(int index) { return rows_[index]; }
+  Vector3 col(int index) const {
+    return Vector3(rows_[0][index], rows_[1][index], rows_[2][index]);
+  }
+  bool operator==(const Matrix3 &other) const;
+  static const Matrix3 kIdentity;
+  static const Matrix3 kZero;
+  static const Matrix3 kOnes;
+  double det() const;
+
+private:
+  Vector3 rows_[3];
+};
+
+inline Matrix3 operator*(const double &lhs, const Matrix3 &rhs) {
+  return (rhs * lhs);
+}
+
+inline std::ostream &operator<<(std::ostream &ss, const Matrix3 &vec) {
+  return ss << "[[" << vec.row(0)[0] << ", " << vec.row(0)[1] << ", "
+            << vec.row(0)[2] << "],"
+            << " [" << vec.row(1)[0] << ", " << vec.row(1)[1] << ", "
+            << vec.row(1)[2] << "],"
+            << " [" << vec.row(2)[0] << ", " << vec.row(2)[1] << ", "
+            << vec.row(2)[2] << "]]";
 }
 
 } // namespace cppcourse
